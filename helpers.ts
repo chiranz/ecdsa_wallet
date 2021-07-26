71a3ddce43fc5f2946a6c7980a28d6c56b4d0df2
chiranz <chiranpoudyal@gmail.com>
2021-07-27 05:16:08 +0530

    refactored ecdsa helper functions

--- helpers.ts
+++ helpers.ts
@@ -66,13 +66,35 @@ export const signTransaction = ({
     s: signature.s.toString(16),
   };
 };
-type SignedTxn = {
+interface ISignaturePub {
   r: string;
   s: string;
   publicKey: string;
+}
+interface SignedTxn extends ISignaturePub {
   to: string;
   amount: number;
   fee: number;
+}
+export const verifyData = (
+  publicKey: string,
+  data: {},
+  r: string,
+  s: string
+): boolean => {
+  const ec = new EC("secp256k1");
+
+  const key = ec.keyFromPublic(publicKey, "hex");
+
+  const dataHash = SHA256(JSON.stringify(data)).toString();
+
+  const signature = {
+    r,
+    s,
+  };
+
+  const verified = key.verify(dataHash, signature);
+  return verified;
 };
 
 export const verifyTransaction = ({
@@ -83,19 +105,61 @@ export const verifyTransaction = ({
   amount,
   fee,
 }: SignedTxn): boolean => {
+  const data = { to, amount, fee };
+  return verifyData(publicKey, data, r, s);
+};
+
+interface IBlockData extends IBlock {
+  privateKey?: string;
+}
+interface ISignedBlock extends ISignaturePub, IBlockData {}
+export const signBlock = ({
+  nonce,
+  transactions,
+  timestamp,
+  hash,
+  privateKey,
+}: IBlockData): ISignedBlock => {
   const ec = new EC("secp256k1");
+  const key = ec.keyFromPrivate(privateKey || "");
+  const publicKey = key.getPublic().encode("hex", true).toString();
 
-  const key = ec.keyFromPublic(publicKey, "hex");
-
-  const txn = { to, amount, fee };
-  const txnHash = SHA256(JSON.stringify(txn)).toString();
-
-  const signature = {
-    r,
-    s,
+  // TODO: change this message to whatever you would like to sign
+  const txn = { timestamp, transactions, nonce, hash };
+  const txnHash = SHA256(JSON.stringify(txn));
+  const signature = key.sign(txnHash.toString());
+  console.log({
+    timestamp,
+    transactions,
+    nonce,
+    hash,
+    publicKey,
+    signature: {
+      r: signature.r.toString(16),
+      s: signature.s.toString(16),
+    },
+  });
+  return {
+    timestamp,
+    transactions,
+    nonce,
+    hash,
+    publicKey,
+    r: signature.r.toString(16),
+    s: signature.s.toString(16),
   };
+};
 
-  const verified = key.verify(txnHash, signature);
-  console.log(verified);
-  return verified;
+export const verifyBlock = ({
+  r,
+  s,
+  publicKey,
+  nonce,
+  transactions,
+  timestamp,
+  hash,
+}: ISignedBlock): boolean => {
+  const data = { timestamp, transactions, nonce, hash };
+
+  return verifyData(publicKey, data, r, s);
 };
